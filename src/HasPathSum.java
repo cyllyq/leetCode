53ba1be9fbdd71378a5190a75ee85a2092ccf5cc
cyllyq <284870342@qq.com>
2021-04-08 18:28:37 +0800

    test

---
+++ src/HasPathSum.java
@@ -0,0 +1,20 @@
+/**
+ * 给你二叉树的根节点root 和一个表示目标和的整数targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和targetSum 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+public class HasPathSum {
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
